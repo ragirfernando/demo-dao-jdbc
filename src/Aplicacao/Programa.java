63ad9f5dc7ac7f54beafcf61510c03c063b09fc6
Ragir Fernando <ragirfernando@gmail.com>
2019-08-13 23:46:59 -0300

    217. Classe Department

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,8 +1,13 @@
 package Aplicacao;
 
+import Model.entidades.Departamento;
+
 public class Programa {
 
     public static void main(String[] args) {
+            
+        Departamento obj = new Departamento(1, "Books");
+        System.out.println(obj);
         
     }
 }
---
+++ src/Model/entidades/Departamento.java
@@ -0,0 +1,66 @@
+package Model.entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Departamento implements Serializable{
+
+    private Integer id;
+    private String name;
+
+    public Departamento() {
+
+    }
+
+    public Departamento(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Departamento other = (Departamento) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento[" + "id = " + id + ", name = " + name + ']';
+    }
+
+}
