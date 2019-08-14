66e80b6a216cc3f58bd0c2612269f713ee6e736f
Ragir Fernando <ragirfernando@gmail.com>
2019-08-13 23:59:26 -0300

    Classe Vendedor

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,13 +1,17 @@
 package Aplicacao;
 
 import Model.entidades.Departamento;
+import Model.entidades.Vendedor;
+import java.util.Date;
 
 public class Programa {
 
     public static void main(String[] args) {
             
         Departamento obj = new Departamento(1, "Books");
-        System.out.println(obj);
+        //System.out.println(obj);
         
+        Vendedor vendedor = new Vendedor(21, "Ragir", "ragirfernando@gmail.com ", new Date(), 3000.00, obj);
+        System.out.println(vendedor);
     }
 }
---
+++ src/Model/entidades/Vendedor.java
@@ -0,0 +1,107 @@
+package Model.entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Vendedor implements Serializable {
+
+    private Integer id;
+    private String nome;
+    private String email;
+    private Date birthDate;
+    private Double baseSlary;
+
+    private Departamento departamento;
+
+    public Vendedor() {
+    }
+
+    public Vendedor(Integer id, String nome, String email, Date birthDate, Double baseSlary, Departamento departamento) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSlary = baseSlary;
+        this.departamento = departamento;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getBaseSlary() {
+        return baseSlary;
+    }
+
+    public void setBaseSlary(Double baseSlary) {
+        this.baseSlary = baseSlary;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vendedor other = (Vendedor) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Vendedor[" + "id = " + id + ", nome = " + nome + ", email = " + email + ", birthDate = " + birthDate + ", baseSlary = " + baseSlary + ", departamento = " + departamento + ']';
+    }
+
+}
